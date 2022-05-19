14671f4f611b7b8995a489b762b42ddff2557cd5
hBTPS <hadrien.bontemps@ensta-bretagne.org>
2022-05-19 21:36:46 +0200

    TP1 Activity 2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8">
     <title>@yield('title','Home Page')</title>
     <!-- Styles -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-
-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
     <link href="{{ asset('css/customStyle.css') }}" rel="stylesheet">
 </head>
 <body>

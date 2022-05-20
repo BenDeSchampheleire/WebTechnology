9c19a51880aab7189771f8615e3298f9d57d0581
hBTPS <hadrien.bontemps@ensta-bretagne.org>
2022-05-20 17:03:41 +0200

    TP1 Activity 4

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,10 @@ class ProductController extends Controller
         $data = []; //to be sent to the view
         $listProducts = array();
         $listProducts[121] = array("name"=>"Tv samsung", "price"=>"1000");
+        if (!key_exists($id, $listProducts)) {
+            return redirect()->route('home.index');
+        }
+
         $listOfSizes = array("XS","S","M","L","XL");
         $data["title"] = $listProducts[$id]["name"];
         $data["product"] = $listProducts[$id];

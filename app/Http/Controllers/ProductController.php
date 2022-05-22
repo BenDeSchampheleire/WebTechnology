495421b8bf3866ecb1f67cfdbdfcdb5fe6359488
hBTPS <hadrien.bontemps@ensta-bretagne.org>
2022-05-22 15:47:12 +0200

    TP1 Activity 7

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
 class ProductController extends Controller
 {
     public function show($id)
@@ -16,5 +18,22 @@ class ProductController extends Controller
         $data["product"] = $listProducts[$id];
         $data["sizes"] = $listOfSizes;
         return view('product.show')->with("data",$data);
+    }
+
+    public function create()
+    {
+        $data = []; //to be sent to the view
+        $data["title"] = "Create product";
+        return view('product.create')->with("data",$data);
+    }
+
+    public function save(Request $request)
+    {
+        $request->validate([
+            "name" => "required",
+            "price" => "required | gt:0",
+        ]);
+        dd($request->all());
+        //here goes the code to call the model and save it to the database
     }
 }

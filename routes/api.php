46fcbbbe39dbadca208f54918c5bc790cf4ac063
hBTPS <hadrien.bontemps@ensta-bretagne.org>
2022-05-25 17:32:12 +0200

    Workshop 4 Activity 1

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', 'App\Http\Controllers\Api\ProductApi@index')->name("api.product.index");
+Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductApi@show')->name("api.product.show");
+
+Route::get('/v2/products', 'App\Http\Controllers\Api\ProductApiV2@index')->name("api.v2.product.index");
+Route::get('/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiV2@show')->name("api.v2.product.show");
+
+Route::get('/v3/products', 'App\Http\Controllers\Api\ProductApiV3@index')->name("api.v3.product.index");
+Route::get('/v3/products/paginate', 'App\Http\Controllers\Api\ProductApiV3@paginate')->name("api.v3.product.paginate");
